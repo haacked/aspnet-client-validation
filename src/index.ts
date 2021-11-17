acaf129d09f83f82dc85261393d50bfa3160dfca
Søren Kottal <sk@ecreo.dk>
2021-11-17 14:14:37 +0100

    adds isValid(form) method for checking whether a form is valid

--- src/index.ts
+++ src/index.ts
@@ -603,6 +603,23 @@ export class ValidationService {
     }
 
     /**
+     * Returns true if the provided form is valid, and then calls the callback. The form will be validated before checking, unless prevalidate is set to false
+     * @param form 
+     * @param prevalidate 
+     * @param callback 
+     * @returns 
+     */
+    isValid = (form: HTMLFormElement, prevalidate: boolean = true, callback: Function) => {
+        if (prevalidate) {
+            this.validateForm(form, callback);
+        }
+        let formUID = this.getElementUID(form);
+        let formInputUIDs = this.formInputs[formUID];
+        let invalidFormInputUIDs = formInputUIDs.filter(uid => this.summary[uid]);
+        return invalidFormInputUIDs.length == 0;
+    }
+
+    /**
      * Tracks a <form> element as parent of an input UID. When the form is submitted, attempts to validate the said input asynchronously.
      * @param form 
      * @param inputUID 
