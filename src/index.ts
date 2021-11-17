968c9d75882a88e9a13cb08933269fd8f43b859f
Søren Kottal <sk@ecreo.dk>
2021-11-17 15:16:45 +0100

    adds isFieldValid(field) method for checking whether a field is valid

--- src/index.ts
+++ src/index.ts
@@ -620,6 +620,26 @@ export class ValidationService {
     }
 
     /**
+     * Returns true if the provided field is valid, and then calls the callback. The form will be validated before checking, unless prevalidate is set to false
+     * @param form 
+     * @param prevalidate 
+     * @param callback 
+     * @returns 
+     */
+    isFieldValid = (field: HTMLElement, prevalidate: boolean = true, callback: Function) => {
+        
+        if (prevalidate) {
+            let form = field.closest("form");
+            if (form != null) {
+                this.validateForm(form, callback);
+            }
+        }
+
+        let fieldUID = this.getElementUID(field);
+        return this.summary[fieldUID] != null;
+    }
+
+    /**
      * Tracks a <form> element as parent of an input UID. When the form is submitted, attempts to validate the said input asynchronously.
      * @param form 
      * @param inputUID 
