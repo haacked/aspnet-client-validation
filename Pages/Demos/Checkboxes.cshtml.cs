7a1aea2b8554a1e82585e48299bde08a3b2fc552
Phil Haack <haacked@gmail.com>
2023-07-03 10:03:22 -0700

    Implement demo page for checkboxes
    To help validate it works correctly.

---
+++ Pages/Demos/Checkboxes.cshtml.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DemoWeb.Pages.Demos;
+
+public class Checkboxes : PageModel
+{
+    public string? StatusMessage { get; set; }
+
+    public IActionResult OnPost()
+    {
+        StatusMessage = "Form was submitted: " + (ModelState.IsValid
+            ? "Model is valid"
+            : "Model is invalid");
+
+        return Page();
+    }
+
+    [BindProperty]
+    public InputModel Input { get; set; } = new();
+
+    [BindProperty]
+    [Required]
+    public List<string> SelectedAnimals { get; set; } = new();
+
+    public IReadOnlyList<string> Animals = new List<string> { "Dog", "Cat", "Fish" };
+
+    public class InputModel
+    {
+        public bool IsChecked { get; set; }
+    }
+}
