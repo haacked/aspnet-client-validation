b62f5194be57171b4ab090703d22d184895db6aa
Jordan Cannon <LiteracyFanatic@gmail.com>
2023-07-28 00:57:28 -0500

    Fix logic of isFieldValid by checking if equal to undefined rather than not equal to null

--- src/index.ts
+++ src/index.ts
@@ -788,7 +788,7 @@ export class ValidationService {
         }
 
         let fieldUID = this.getElementUID(field);
-        return this.summary[fieldUID] != null;
+        return this.summary[fieldUID] === undefined;
     }
 
     /**
